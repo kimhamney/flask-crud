4db18d17c8a36691d55c3cb7fa5d5b3bcb4057e4
kimhamney <kimhamney@gmail.com>
2024-01-05 15:04:30 +0900

    add list, signup function

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!'
+def index():
+    return redirect(url_for('question._list'))
